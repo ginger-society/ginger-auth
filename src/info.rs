25fccbf80c43d8c9ee496899d0c58a6ece5b7b26
Ginger Society Admin <admin@gingersociety.org>
2026-04-27 21:40:53 +0530

    fix: pipeline

--- src/info.rs
+++ src/info.rs
@@ -1,9 +1,9 @@
-use IAMService::{apis::default_api::identity_validate_token, get_configuration};
+use IAMService::{apis::default_api::{identity_validate_api_token}, get_configuration};
 
 pub async fn get_session_info() {
     let iam_config = get_configuration(None);
     println!("{:?}", iam_config);
-    match identity_validate_token(&iam_config).await {
+    match identity_validate_api_token(&iam_config).await {
         Ok(profile_response) => {
             println!("{:?}", profile_response)
         }
